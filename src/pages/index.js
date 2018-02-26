24ea85849efa85465fa00796dbab2c5d1b7072ae
alex <am3081@gmail.com>
2018-02-26 17:10:04 -0500

    test 123

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,9 @@ import Screen from '../components/Screen'
 
 const IndexPage = () => (
   <section className='content'>
-      <div className='heroImage' style={{
-          width: '100%',
-          height: '50vh',
-          border: '1px solid black',
-          backgroundImage: "url(//cdn.shopify.com/s/files/1/0097/0772/files/cover-back_c2e4aeb2-a851-49cd-9896-e37f7e291b0f_2000x.png)"}}>
-      </div>
-      <div className='home-container'>
-        test 123
-      </div>
+    <h1 className='nav-link' style={{paddingTop: '25%'}}>
+      COMING SOON...
+    </h1>
   </section>
 )
 
